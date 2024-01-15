eeb44b1ad528da7ae8964419079c74e7969a3610
Alao Jessica <alaojessica747@gmail.com>
2024-01-15 11:01:38 +0100

    compete sign up page

--- mernproject/src/pages/SignUp.jsx
+++ mernproject/src/pages/SignUp.jsx
@@ -1,9 +1,11 @@
 import { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link,useNavigate } from "react-router-dom";
 
 const SignUp = () => {
   const [formData, setFormData] = useState({});
-
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const navigate=useNavigate()
   const handleChange = (e) => {
     setFormData({
       ...formData,
@@ -13,16 +15,30 @@ const SignUp = () => {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+    setLoading(true);
 
-    const res = await fetch("http://localhost:4000/api/auth/signup", {
-      method: "post",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(formData),
-    });
-    const data = await res.json();
-    console.log(data);
+    try {
+      const res = await fetch("http://localhost:4000/api/auth/signup", {
+        method: "post",
+
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        setError(data.message);
+        setLoading(false);
+        return;
+      }
+      setLoading(false);
+      setError(null);
+      navigate('/sign-in')
+    } catch (error) {
+      setLoading(false);
+      setError(error.message);
+    }
   };
   console.log(formData);
   return (
@@ -50,8 +66,11 @@ const SignUp = () => {
           id="password"
           onChange={handleChange}
         />
-        <button className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80">
-          Sign Up
+        <button
+          disabled={loading}
+          className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80"
+        >
+          {loading ? "loading... " : "Sign up"}
         </button>
       </form>
 
@@ -61,6 +80,8 @@ const SignUp = () => {
           <span className="text-blue-700">Sign In</span>
         </Link>
       </div>
+
+      {error && <p className="text-red-500 mt-5">{error}</p>}
     </div>
   );
 };
